42e9db7e0eaca2aaec51318fd5791a49c38a21bc
shecannotsee <shecannotsee@163.com>
2023-05-15 15:07:31 +0800

    [feat]添加一个类生成的实现

---
+++ src/cpp_class_generated.py
@@ -0,0 +1,45 @@
+# 拷贝和移动参考
+# class vector {
+# private:
+#     // ......
+# public:
+# vector() = default;
+# ~vector() = default;
+# // copy
+# vector(const vector& x) = default;
+# vector& operator=(const vector& x) = default;
+# // move
+# vector(vector&&) = default;
+# vector& operator=(vector&& x) = default;
+# public:
+# // ......
+# };
+import sys
+
+# 获取命令行参数
+class_name = sys.argv[1] if len(sys.argv) > 1 else input("input class name：")
+
+code = ""
+code += "class "+ class_name + " {\n"
+code += " public:\n"
+code += "  // copy:off\n"
+code += "  "+class_name+"(const "+class_name+"&) = delete;\n"
+code += "  "+class_name+"& operator=(const "+class_name+"&) = delete;\n"
+code += "  // move:off\n"
+code += "  "+class_name+"("+class_name+"&&) = delete;\n"
+code += "  "+class_name+"& operator=("+class_name+"&&) = delete;\n"
+code += "  //destructors\n"
+code += "  ~"+class_name+"();\n"
+code += "  //constructors\n"
+code += "  "+class_name+"();\n"
+code += " private:\n"
+code += "  // data\n"
+code += " public:\n"
+code += "  //interface\n"
+code += "\n"
+code += "};"
+
+print(code)
+
+
+
