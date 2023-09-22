062cfb8db7ca268e0c54ebe260542b43fe5f3751
shecannotsee <shecannotsee@163.com>
2023-09-22 16:56:05 +0800

    [feat] read modify

--- src/cmake_module_generate/CMakeLists_generate.py
+++ src/cmake_module_generate/CMakeLists_generate.py
@@ -2,7 +2,7 @@ import sys
 import textwrap
 
 # 获取命令行参数
-# class_name = sys.argv[1] if len(sys.argv) > 1 else input("input class name：")
+# module_name = sys.argv[1] if len(sys.argv) > 1 else input("Additional modules name(default exec)：")
 
 code = textwrap.dedent('''
 cmake_minimum_required(VERSION 3.11)
--- src/cmake_module_generate/target_exec_generate.py
+++ src/cmake_module_generate/target_exec_generate.py
@@ -2,7 +2,7 @@ import sys
 import textwrap
 
 # 获取命令行参数
-module_name = sys.argv[1] if len(sys.argv) > 1 else input("请输入执行模块名称：")
+module_name = sys.argv[1] if len(sys.argv) > 1 else input("Additional modules name(default exec)：")
 
 code = textwrap.dedent(f"""
 ############################################################################################################
