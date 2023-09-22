eaa1e042caf5a73c01c6e35d135696a7381790a5
shecannotsee <shecannotsee@163.com>
2023-09-22 16:09:02 +0800

    [feat] modify module

--- src/cmake_module_generate/config_generate.py
+++ src/cmake_module_generate/config_generate.py
@@ -1,7 +1,7 @@
 import sys
 
 # 获取命令行参数
-# class_name = sys.argv[1] if len(sys.argv) > 1 else input("input class name：")
+module_name = sys.argv[1] if len(sys.argv) > 1 else input("Additional modules name(default exec)：")
 
 LF = "\n"
 code = ""
@@ -26,9 +26,9 @@ code += "# test target" + LF
 code += "set(test_name \"${project_name}_test\")" + LF
 code += "set(generate_test ON)" + LF
 code += LF
-code += "# exec target" + LF
-code += "set(exec_name \"${project_name}_exec\")" + LF
-code += "set(generate_exec ON)" + LF
+code += "# " + module_name + " target" + LF
+code += "set(" + module_name + "_name \"${project_name}_" + module_name + "\")" + LF
+code += "set(generate_" + module_name + " ON)" + LF
 code += LF
 
 print(code)
