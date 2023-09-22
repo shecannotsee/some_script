0e7982e81674c23a45004c22e4057e036537f2d4
shecannotsee <shecannotsee@163.com>
2023-09-22 16:27:44 +0800

    [feat] easy to read and modify

--- src/cmake_module_generate/src_build_generate.py
+++ src/cmake_module_generate/src_build_generate.py
@@ -1,4 +1,5 @@
 import sys
+import textwrap
 
 # 获取命令行参数
 # class_name = sys.argv[1] if len(sys.argv) > 1 else input("input class name：")
@@ -12,32 +13,28 @@ code += "# src build                                                            
 code += "############################################################################################################" \
         "############" + LF
 
-code += "# source src" + LF
-code += "file(GLOB_RECURSE SRC \"${CMAKE_SOURCE_DIR}/src/*\")" + LF
-code += LF
-code += "# Exclude specific files" + LF
-code += "# list(REMOVE_ITEM SRC \"${CMAKE_SOURCE_DIR}/src/base64.cpp\")" + LF
-code += LF
-code += "# src dependency" + LF
-code += "set(src_dependency \"-pthread\"" + LF
-code += "                   )" + LF
-code += LF
-code += "# src build to lib" + LF
-code += "if (generate_lib STREQUAL \"ON\")" + LF
-code += "    add_library(${library_static_name} STATIC ${SRC})" + LF
-code += "    # Rename the generated static library" + LF
-code += "    set_target_properties(${library_static_name} PROPERTIES OUTPUT_NAME ${project_name})" + LF
-code += LF
-code += "    add_library(${library_dynamic_name} SHARED ${SRC})" + LF
-code += "    # Rename the generated dynamic library" + LF
-code += "    set_target_properties(${library_dynamic_name} PROPERTIES OUTPUT_NAME ${project_name})" + LF
-code += ""
-code += "else()" + LF
-code += "    message(STATUS \"CMakeLists.txt error:src build error\")" + LF
-code += "endif ()" + LF
-code += LF
+code += textwrap.dedent('''
+# source src
+file(GLOB_RECURSE SRC \"${CMAKE_SOURCE_DIR}/src/*\")
+# Exclude specific files
+# list(REMOVE_ITEM SRC "${CMAKE_SOURCE_DIR}/src/base64.cpp")
 
+# src dependency
+set(src_dependency "-pthread")
 
+# src build to lib
+if (generate_lib STREQUAL "ON")
+    add_library(${library_static_name} STATIC ${SRC})
+    # Rename the generated static library
+    set_target_properties(${library_static_name} PROPERTIES OUTPUT_NAME ${project_name})
+    
+    add_library(${library_dynamic_name} SHARED ${SRC})
+    # Rename the generated dynamic library
+    set_target_properties(${library_dynamic_name} PROPERTIES OUTPUT_NAME ${project_name})
+else()
+    message(STATUS "CMakeLists.txt error:src build error")
+endif ()
+''')
 code += LF
 
 print(code)
