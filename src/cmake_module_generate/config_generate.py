cde7fc14cad3ad77ce2d436edb8964c286e57daf
shecannotsee <shecannotsee@163.com>
2023-09-22 16:20:52 +0800

    [feat] easy to read and modify

--- src/cmake_module_generate/config_generate.py
+++ src/cmake_module_generate/config_generate.py
@@ -1,4 +1,5 @@
 import sys
+import textwrap
 
 # 获取命令行参数
 module_name = sys.argv[1] if len(sys.argv) > 1 else input("Additional modules name(default exec)：")
@@ -11,21 +12,23 @@ code += "# Global Settings                                                      
         "           #" + LF
 code += "############################################################################################################" \
         "############" + LF
-code += "# project name" + LF
-code += "set(project_name \"project_name\")" + LF
-code += LF
-code += "# [Release] or [Debug] or [MinSizeRel]" + LF
-code += "set(compilation_options \"Debug\")" + LF
-code += LF
-code += "# src target" + LF
-code += "set(library_static_name \"${project_name}_static\")" + LF
-code += "set(library_dynamic_name \"${project_name}_dynamic\")" + LF
-code += "set(generate_lib ON)" + LF
-code += LF
-code += "# test target" + LF
-code += "set(test_name \"${project_name}_test\")" + LF
-code += "set(generate_test ON)" + LF
-code += LF
+
+code += textwrap.dedent('''
+# project name
+set(project_name "project_name")
+# [Release] or [Debug] or [MinSizeRel]
+set(compilation_options "Debug")
+
+# src target
+set(library_static_name "${project_name}_static")"
+set(library_dynamic_name "${project_name}_dynamic")"
+set(generate_lib ON)"
+
+# test target
+set(test_name "${project_name}_test")
+set(generate_test ON)
+
+''')
 code += "# " + module_name + " target" + LF
 code += "set(" + module_name + "_name \"${project_name}_" + module_name + "\")" + LF
 code += "set(generate_" + module_name + " ON)" + LF
