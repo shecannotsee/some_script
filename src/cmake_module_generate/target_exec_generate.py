f1cd9126b7d4668a318eef255183311da9dc1897
shecannotsee <shecannotsee@163.com>
2023-09-22 16:31:42 +0800

    [feat] easy to read and modify

--- src/cmake_module_generate/target_exec_generate.py
+++ src/cmake_module_generate/target_exec_generate.py
@@ -1,36 +1,31 @@
 import sys
 
 # 获取命令行参数
-module_name = sys.argv[1] if len(sys.argv) > 1 else input("input exec module name：")
+module_name = sys.argv[1] if len(sys.argv) > 1 else input("请输入执行模块名称：")
 
 LF = "\n"
 code = ""
-code += "############################################################################################################" \
-        "############" + LF
-code += "# " + module_name + " target build " + LF
-code += "############################################################################################################" \
-        "############" + LF
+code += (f"""
+############################################################################################################
+# {module_name} target build
+############################################################################################################
 
-code += "# " + module_name + " src" + LF
-code += "file(GLOB_RECURSE " + module_name + "_SRC \"${CMAKE_SOURCE_DIR}/" + module_name + "/*\")" + LF
-code += LF
-code += "# Exclude specific files" + LF
-code += "# list(REMOVE_ITEM " + module_name + "_SRC \"${CMAKE_SOURCE_DIR}/" + module_name + "/base64.cpp\")" + LF
-code += LF
-code += "# " + module_name + " dependency" + LF
-code += "set(" + module_name + "_dependency \"-pthread\"" + LF
-code += "                      )" + LF
-code += LF
-code += "# " + module_name + " target" + LF
-code += "if (generate_" + module_name + " STREQUAL \"ON\")" + LF
-code += "    add_executable(${" + module_name + "_name} ${" + module_name + "_SRC} ${SRC})" + LF
-code += "    target_link_libraries(${" + module_name + "_name} ${lib} ${src_dependency} ${" + module_name + "_dependency})" + LF
-code += "else()" + LF
-code += "    message(STATUS \"CMakeLists.txt error:" + module_name + " target build error\")" + LF
-code += "endif ()" + LF
-code += LF
+# {module_name} src
+file(GLOB_RECURSE {module_name}_SRC "${{CMAKE_SOURCE_DIR}}/{module_name}/*")
+# Exclude specific files
+# list(REMOVE_ITEM {module_name}_SRC "${{CMAKE_SOURCE_DIR}}/{module_name}/base64.cpp")
 
+# {module_name} dependency
+set({module_name}_dependency "-pthread")
 
+# {module_name} target
+if (generate_{module_name} STREQUAL "ON")
+    add_executable(${{{module_name}_name}} ${{{module_name}_SRC}} ${{SRC}})
+    target_link_libraries(${{{module_name}_name}} ${{lib}} ${{src_dependency}} ${{{module_name}_dependency}})
+else()
+    message(STATUS "CMakeLists.txt error: {module_name} target build error")
+endif ()
+""")
 code += LF
 
 print(code)
