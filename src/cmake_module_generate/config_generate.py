37f3dab717943bfb517769ecace6508a4becbfc8
shecannotsee <shecannotsee@163.com>
2023-09-25 12:59:55 +0800

    [fix] config generate modify

--- src/cmake_module_generate/config_generate.py
+++ src/cmake_module_generate/config_generate.py
@@ -15,9 +15,9 @@ set(project_name "placeholder_name")
 set(compilation_options "Debug")
 
 # src target
-set(library_static_name "${{project_name}}_static")"
-set(library_dynamic_name "${{project_name}}_dynamic")"
-set(generate_lib ON)"
+set(library_static_name "${{project_name}}_static")
+set(library_dynamic_name "${{project_name}}_dynamic")
+set(generate_lib ON)
 
 # test target
 set(test_name "${{project_name}}_test")
