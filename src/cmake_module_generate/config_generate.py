9a6ab6fa7c6c288a5c64fcb7fc6e205e130d3f58
shecannotsee <shecannotsee@163.com>
2023-10-13 10:47:14 +0800

    [fix] process cmake warning

--- src/cmake_module_generate/config_generate.py
+++ src/cmake_module_generate/config_generate.py
@@ -9,19 +9,7 @@ code = textwrap.dedent(f'''
 # Global Settings                                                                                                      #
 ########################################################################################################################
 
-# project name
-set(project_name "placeholder_name")
-PROJECT(${{project_name}}
-        VERSION      0.0.1
-        LANGUAGES    CXX
-        )
-
-# [Release] or [Debug] or [MinSizeRel]
-set(compilation_options "Debug")
-
 # src target
-set(library_static_name "${{project_name}}_static")
-set(library_dynamic_name "${{project_name}}_dynamic")
 set(generate_lib ON)
 
 # test target
