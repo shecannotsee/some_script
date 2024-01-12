02aa0d308b0a00221d474f7881ad67f4c7f967de
shecannotsee <shecannotsee@163.com>
2024-01-12 17:05:23 +0800

    [feat] support no timeout system

--- src/gtest_support/run_all_suite.sh
+++ src/gtest_support/run_all_suite.sh
@@ -4,7 +4,7 @@ exec_program="shared_test_test"
 ############# 获取--gtest_list_tests输出的每一行并组成--gtest_filter的用例 #############
 output=$(./${exec_program} --gtest_list_tests)
 formatted_output=$(echo "${output}" | sed ':a;N;$!ba;s/\n\(\S\)/ \1/g')
-items=($(echo "${formatted_output}" | grep -oP '\S+'))
+items=($(echo "${formatted_output}" | grep -o '\S\+'))
 
 suite_name=()
 now_father_suite=""
@@ -24,13 +24,18 @@ done
 ############# 单独运行每一个测试用例 #############
 error_suite=()
 for ((i=0; i<${#suite_name[@]}; i++)); do
-    running_output=$(timeout 1s ./${exec_program} --gtest_filter=${suite_name[i]})
+    ./${exec_program} --gtest_filter=${suite_name[i]} > output_file.txt 2>&1 &
+    pid=$!
+    (sleep 1 && kill -9 $pid) 2>/dev/null &
+    wait $pid
+    running_output=$(cat output_file.txt)
     if [ $? -eq 0 ] && echo "$running_output" | grep -q "PASSED"; then
       :
     else
       error_suite+=("./${exec_program} --gtest_filter=${suite_name[i]}")
     fi
 done
+
 
 ############# 报错 #############
 for ((i=0; i<${#error_suite[@]}; i++)); do
