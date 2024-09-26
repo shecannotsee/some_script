eec9322ee4a4b970319a64a2ae31a79c74947f11
shecannotsee <shecannotsee@163.com>
2024-09-26 23:50:02 +0800

    [feat] 1.Add TODO

--- src/option/o3.py
+++ src/option/o3.py
@@ -3,4 +3,5 @@ from typing import NoReturn
 from option.file import get_content
 
 def cpp_project_generation() -> NoReturn:
+    # TODO: To be implemented
     print("C++ project generation")
--- src/option/o4.py
+++ src/option/o4.py
@@ -3,4 +3,5 @@ from typing import NoReturn
 from option.file import get_content
 
 def mock_sys_function() -> NoReturn:
+    # TODO: To be implemented
     print("mock_sys_function")
