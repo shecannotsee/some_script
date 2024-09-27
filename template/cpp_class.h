5decec4d473712b373a872dda0c75f06c6bbf582
shecannotsee <shecannotsee@163.com>
2024-09-27 09:42:19 +0800

    [feat] 1.Add template

---
+++ template/cpp_class.h
@@ -0,0 +1,21 @@
+class afa05a6348902c252ae10d48879826b3 {
+ private:
+  // data
+
+ public:
+  // constructors
+  afa05a6348902c252ae10d48879826b3() = default;
+  // destructors
+  ~afa05a6348902c252ae10d48879826b3() = default;
+  // copy:off
+  afa05a6348902c252ae10d48879826b3(const afa05a6348902c252ae10d48879826b3&) = delete;
+  afa05a6348902c252ae10d48879826b3& operator=(const afa05a6348902c252ae10d48879826b3&) = delete;
+  // move:off
+  afa05a6348902c252ae10d48879826b3(afa05a6348902c252ae10d48879826b3&&) = delete;
+  afa05a6348902c252ae10d48879826b3& operator=(afa05a6348902c252ae10d48879826b3&&) = delete;
+
+ private:
+  // Internal function
+ public:
+  // interface
+};
