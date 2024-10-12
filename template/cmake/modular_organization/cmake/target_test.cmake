400439f95f20983515e184345a9623de9d4776a9
shecannotsee <shecannotsee@163.com>
2024-10-12 11:07:24 +0800

    [fix] 1.remove other main.cpp

--- template/cmake/modular_organization/cmake/target_test.cmake
+++ template/cmake/modular_organization/cmake/target_test.cmake
@@ -1,5 +1,6 @@
 if (build_tests STREQUAL ON)
     file(GLOB_RECURSE test_src "${CMAKE_SOURCE_DIR}/test/*")
+    list(REMOVE_ITEM other_src "${CMAKE_SOURCE_DIR}/other/main.cpp")
     add_executable(${test_name} ${src} ${other_src} ${test_src})
     list(APPEND test_link_dependency ${src_link_dependency} ${other_link_dependency})
     target_link_libraries(${test_name} ${test_link_dependency})
