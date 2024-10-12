33f0c21984f5614842d5822d9d5236648bfb3314
shecannotsee <shecannotsee@163.com>
2024-10-12 11:07:04 +0800

    [feat] 1.Modify the import directory of gtest, refer to some_script/template/cpp_project

--- template/cmake/modular_organization/cmake/import_third_party.cmake
+++ template/cmake/modular_organization/cmake/import_third_party.cmake
@@ -4,7 +4,7 @@ include_directories(${CMAKE_SOURCE_DIR}/test)
 
 ########################################################################################################################
 # set: gtest
-set(_gtest_path "${CMAKE_CURRENT_SOURCE_DIR}/googletest-1.12.0")
+set(_gtest_path "${CMAKE_CURRENT_SOURCE_DIR}/third_party/googletest-1.12.0")
 set(gtest_include_dir "${_gtest_path}/include")
 set(gtest_lib_dir "${_gtest_path}/lib")
 set(gtest_link_libs gmock gtest)
